18538d4ab7be3625901e21b27a0027571662f4af
Peter Tucker <ptucker@whitworth.edu>
2018-05-30 16:10:59 -0700

    Added feedback

--- EX03/EX03_02/QuadraticEquation.cpp
+++ EX03/EX03_02/QuadraticEquation.cpp
@@ -1,5 +1,8 @@
 #include "QuadraticEquation.h"
 #include <cmath>
+
+//PT -- 17/20
+
 void QuadraticEquation::Set(double Newa, double Newb, double Newc)	//could not use the class as the type
 {
 	a = Newa;
@@ -7,6 +10,10 @@ void QuadraticEquation::Set(double Newa, double Newb, double Newc)	//could not u
 	c = Newc;
 }
 
+//PT -- missing getA(), getB(), and getC(). You have a, b, c as public, which is ok, but bad practice.
+
+//PT -- Function should use member variables, not parameters.
+// -2
 double QuadraticEquation::GetDiscriminant(double a, double b, double c)
 {
 	Discriminant = (pow(b, 2) - 4 * a*c);
@@ -16,6 +23,9 @@ double QuadraticEquation::GetDiscriminant(double a, double b, double c)
 double QuadraticEquation::GetRoot1(double a, double b, double c)
 {
 	double x;
+	//PT -- Instead of using the member variable, call GetDiscriminant().
+	//      You're relying on the user to call GetDiscriminant before calling GetRoot1().
+	// -1
 	x = ((-b - sqrt(Discriminant)) / (2 * a));
 	return (x);
 }
