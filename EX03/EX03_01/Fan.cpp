98797507d5c108e8aa4553a91874adcdf301e3f5
MrBean1512 <39568733+MrBean1512@users.noreply.github.com>
2018-05-29 23:31:21 -0700

    Add files via upload

---
+++ EX03/EX03_01/Fan.cpp
@@ -0,0 +1,4 @@
+/*
+	I made this program before our lectures/the book covered making a class.cpp file.
+The progression of each problem should show my gradual understanding as they become
+cleaner and more concise
