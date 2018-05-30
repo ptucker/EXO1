d95e63144feddea662eb596d7d0960f94c097d77
Peter Tucker <ptucker@whitworth.edu>
2018-05-30 16:05:53 -0700

    Added feedback

--- EX03/EX03_01/Fan.h
+++ EX03/EX03_01/Fan.h
@@ -2,6 +2,8 @@
 #ifndef FAN_H_
 #define FAN_H_
 
+//PT -- 19/20
+
 class Fan
 {
 private:		//The book asked for "accessor and mutator functions for all data fields" but only Speed was given any parameters so I decided to do the following
@@ -9,12 +11,16 @@ private:		//The book asked for "accessor and mutator functions for all data fiel
 public:
 	bool On;	//did not include private bool because there are only two possible values already
 	double Radius;	//did not inlude a private Radius because it seemed that there was no set limit to it
+	//PT -- I think a negative value for radius would be bad.
+	//PT -- In general, put member variables in private
 	int GetSpeed=Speed;
 
 	Fan()
 	{
 		Speed = 1;
 		Radius = 5;
+		//PT -- On = false;
+		//PT -- -1
 	}
 
 	void TurnOn()
@@ -38,6 +44,9 @@ public:
 			Speed = NewSpeed;
 		GetSpeed = Speed;
 	}
+	
+	//PT -- int getSpeed() { return speed; }
+	//PT -- the member variable GetSpeed works, but you need to make sure you maintain it, and you don't in Fan().
 };
 
 #endif
