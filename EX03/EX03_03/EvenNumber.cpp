fab57440dd2057d583b23d10b58c65e31e2c4818
Peter Tucker <ptucker@whitworth.edu>
2018-05-30 16:12:24 -0700

    Added feedback

--- EX03/EX03_03/EvenNumber.cpp
+++ EX03/EX03_03/EvenNumber.cpp
@@ -1,10 +1,14 @@
 #include "EvenNumber.h"
+//PT -- 19/20
+
 EvenNumber::EvenNumber()
 {
 	Even = 0;
 }
 EvenNumber::EvenNumber(int NewEven)
 {
+	//PT -- check to be sure newEven is actually even. If not, set Even to 0, or NewEven+1/NewEven-1 (your choice).
+	// -1
 	Even = NewEven;
 }
 int EvenNumber::GetValue()
